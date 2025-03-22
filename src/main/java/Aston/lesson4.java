042f0061e777fb0b0f0122928ed13088dde00f48
A7im <asimvo@gmail.com>
2025-03-22 17:43:36 +0300

    init commit

--- src/main/java/Aston/lesson4.java
+++ src/main/java/Aston/lesson4.java
@@ -3,7 +3,10 @@ package Aston;
 public class lesson4 {
 
     public static void main (String[] args){
+        printThreeWords();
 
-
+    }
+    public static void printThreeWords() {
+        System.out.println(" Orange\n Banana\n Apple\n");
     }
 }
