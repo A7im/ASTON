329b3743d6650cf9b3305700020f919a3e47eba6
A7im <asimvo@gmail.com>
2025-04-02 21:34:46 +0300

    finish

---
+++ src/main/java/Aston/Park.java
@@ -0,0 +1,34 @@
+package Aston;
+
+import java.util.ArrayList;
+
+public class Park {
+
+     ArrayList<Attraction> attractions = new ArrayList<>();
+
+   public static class Attraction {
+        String workTime, name;
+        float price;
+
+    }
+
+    public void addAttraction(String name, String workTime, float price) {
+        Attraction newAttraction = new Attraction();
+        newAttraction.name = name;
+        newAttraction.workTime = workTime;
+        newAttraction.price = price;
+        this.attractions.add(newAttraction);
+
+    }
+
+    public void parkInfo() {
+        for (Attraction any : attractions) {
+            System.out.println("Аттракцион: " +any.name);
+            System.out.print("Работает: " +any.workTime + " ");
+            System.out.println("Цена билета: " +any.price + " руб.");
+
+        }
+
+    }
+
+}
