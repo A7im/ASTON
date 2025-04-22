d60f536693ab8cbb72ac582c89d92e72db228a29
A7im <asimvo@gmail.com>
2025-04-22 19:42:00 +0300

    Change commit

--- src/main/java/Aston/Cat.java
+++ src/main/java/Aston/Cat.java
@@ -3,11 +3,20 @@ package Aston;
 public class Cat extends Animal {
     private static int catCount = 0;
     private boolean isHungry = true;
+
     public Cat(String name) {
         super(150, 0, name);
         catCount++;
-
     }
+
+    public static int getCatCount() {
+        return catCount;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
     public void eat(Bowl bowl, int amount) {
         if (bowl.decreaseFood(amount)) {
             isHungry = false;
@@ -17,48 +26,6 @@ public class Cat extends Animal {
         }
     }
 
-    public boolean isHungry() {
-        return isHungry;
-    }
-
-    public static int getCatCount() {
-        return catCount;
-    }
-
-    static class Bowl {
-        private int foodAmount;
-
-        public Bowl(int food) {
-            if (food < 0)
-                System.out.println("Еда не может быть отрицательным");
-            this.foodAmount = food;
-
-        }
-
-        public boolean decreaseFood(int amount) {
-            if (amount <= 0) {
-                System.out.println("Кол-во еды должно быть положительным");
-                return false;
-            }
-            if (foodAmount >= amount) {
-                foodAmount -= amount;
-                return true;
-            }
-            return false;
-        }
-
-        public void addFood(int amount) {
-            if (amount > 0) {
-                foodAmount += amount;
-                System.out.println("В миску добавлено " + amount + " еды. Теперь в миске " + foodAmount + " еды.");
-            } else {
-                System.out.println("Нельзя добавить отрицательное количество еды");
-            }
-        }
-
-        public int getFoodAmount() {
-            return foodAmount;
-        }
-    }
 }
+
 
