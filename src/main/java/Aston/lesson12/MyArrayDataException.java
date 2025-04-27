87f1968057261e55fe6ab866d4efbe3241cee3a2
A7im <asimvo@gmail.com>
2025-04-27 12:35:56 +0300

    Last commit

--- src/main/java/Aston/lesson12/MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-package Aston.lesson12;
-
-public class MyArrayDataException extends Exception {
-
-    public MyArrayDataException(int row, int col, String value) {
-
-        super(String.format("Некорректные данные в указанной ячейке [%d][%d]: '%s'", row, col, value));
-    }
-}
